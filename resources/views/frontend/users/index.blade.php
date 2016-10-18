c7cb95ea947760602342f1d97d78b107d6899864
jimuelpalaca <jimuelpalaca06@gmail.com>
2016-10-18 15:47:24 +0800

    some test

--- resources/views/frontend/users/index.blade.php
+++ resources/views/frontend/users/index.blade.php
@@ -4,5 +4,12 @@
     @include('frontend.users.templates.nav')
 
     <div class="container">
+
+    <script type="text/javascript" src="http://www.google.com/jsapi"></script>
+<script>
+contry_code = google.loader.ClientLocation.address.country_code
+city = google.loader.ClientLocation.address.city
+region = google.loader.ClientLocation.address.region
+</script>
     </div>
 @stop

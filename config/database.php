6d987b95594c00e88fe3fc5599ed40614aeddc27
jimuelpalaca <jimuelpalaca06@gmail.com>
2016-11-03 13:06:58 +0800

    database config to heroku

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'sql6.freemysqlhosting.net',
-            'port' => '3306',
-            'database' => "sql6139126",
-            'username' => "sql6139126",
-            'password' => "AlKmPjjtjM",
+            'host' => 'ec2-23-23-192-249.compute-1.amazonaws.com',
+            'port' => '5432',
+            'database' => "d9mbeh2og1cjca",
+            'username' => "ksahwzejjcsipt",
+            'password' => "v47XabnSNCGsQKa9SK5W2GX3aL",
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',

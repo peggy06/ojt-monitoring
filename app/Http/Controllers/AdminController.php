7fc3d291fa7f971811657c4809ddd18050e7b521
jimuelpalaca <jimuelpalaca06@gmail.com>
2016-11-08 13:52:37 +0800

    update 80%

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -132,7 +132,7 @@ class AdminController extends Controller
                     });
 //                    $sent = true; //temporary email sent
                 }catch (Exception $e){
-                    session()->flash('setup-failed', "Can't send account confirmation to your email address. Please check your network connection and try again.");
+                    session()->flash('setup-failed', "Can't send account confirmation to your email address. Please check your network connection and try again.".$e->getMessage."");
                     return redirect()->back();
                 }
 
